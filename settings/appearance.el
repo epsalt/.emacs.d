438e4e18dde953c71dcd720bfa44c49ad1fa8b95
epsalt <evan.saltman@gmail.com>
2018-03-11 14:37:33 -0600

    fixed theme loading with emacs daemon frames

--- settings/appearance.el
+++ settings/appearance.el
@@ -2,15 +2,25 @@
 (setq visible-bell nil)
 (setq ring-bell-function 'ignore)
 
-(defun my/load-theme (frame)
-  (select-frame frame)
-  (load-theme 'base16-tomorrow-night t))
-(if (daemonp)
-    (add-hook 'after-make-frame-functions #'my/load-theme)
-  (load-theme 'base16-tomorrow-night t))
-
 ;; Fringe
 (fringe-mode 30)
-(set-face-attribute 'fringe nil :background nil)
+(defun my-tone-down-fringes ()
+  (set-face-attribute 'fringe nil
+                      :foreground (face-foreground 'default)
+                      :background (face-background 'default)))
+
+;; Load theme
+(defun my-load-theme ()
+  (load-theme 'base16-tomorrow-night t)
+  (my-tone-down-fringes))
+
+(my-load-theme)
+
+;; Make sure theme is loaded and fringes are hidden on daemon frames
+(defun my-load-theme-frame (frame)
+  (select-frame frame)
+  (my-load-theme))
+(if (daemonp)
+    (add-hook 'after-make-frame-functions #'my-load-theme-frame))
 
 (provide 'appearance)
