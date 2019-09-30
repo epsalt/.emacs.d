5827f63925a2856747d0f2f5d9d5330a9b61779f
epsalt <evan.saltman@gmail.com>
2019-09-30 12:25:24 -0600

    changed python config

--- package-config/use-jedi.el
+++
@@ -1,7 +0,0 @@
-(use-package company-jedi
-  :ensure t
-  :after (company python)
-  :hook (python-mode . jedi-mode)
-  :custom (jedi:complete-on-dot t)
-  :config
-  (add-to-list 'company-backends 'company-jedi))
--- package-config/use-python.el
+++ package-config/use-python.el
@@ -1,10 +1,12 @@
 (use-package python
+  :demand t
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode)
-  :commands python-mode
-  :after (pyvenv)
   :hook (inferior-python-mode . my-python-enable-eldoc-inferior-python)
   :config
+  (setq python-environment-directory (expand-file-name "~/.virtualenvs"))
+  (setq python-shell-interpreter "python3")
+
   (defcustom ipython-shell-interpreter-args "-m IPython --simple-prompt --pprint -i"
     "Default arguments for the IPython interpreter."
     :type 'string
@@ -20,4 +22,19 @@
     (run-python (ipython-shell-calculate-command) nil t))
 
   (defun my-python-enable-eldoc-inferior-python ()
-    (setq-local eldoc-documentation-function #'python-eldoc-function)))
+    (setq-local eldoc-documentation-function #'python-eldoc-function))
+
+  (use-package company-jedi
+    :ensure t
+    :custom
+    (jedi:complete-on-dot t)
+    (jedi:tooltip-method nil)
+    (jedi:setup-keys t)
+    :config
+    (add-hook 'python-mode-hook 'jedi:setup)
+    (add-to-list 'company-backends 'company-jedi)
+
+    (setq jedi:environment-virtualenv
+	  (append python-environment-virtualenv '("--python" "python3"))))
+
+  )
