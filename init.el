073011fc9106a054c9ad8b76072727d8db2daba3
Evan Saltman <evan.saltman@gmail.com>
2019-10-22 21:43:17 -0600

    bootstrap use-package

--- init.el
+++ init.el
@@ -3,6 +3,11 @@
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
+;; Bootstrap use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 (eval-when-compile
   (require 'use-package))
 
