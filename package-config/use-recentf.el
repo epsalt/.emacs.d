b827a6243c44d3700c3bfcb2e3991e63ce16ec86
Evan Saltman <evan.saltman@gmail.com>
2019-12-01 11:18:01 -0700

    fixed recentf config

--- package-config/use-recentf.el
+++ package-config/use-recentf.el
@@ -1,12 +1,17 @@
 (use-package recentf
-:ensure t
-:bind ("C-x C-r" . 'ido-recentf-open)
-:custom (recentf-max-saved-items 150)
-:config
-(defun ido-recentf-open ()
-  "Use `ido-completing-read' to find a recent file."
-  (interactive)
-  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
-      (message "Opening file...")
-    (message "Aborting")))
-)
+  :ensure t
+  :demand t
+  :bind ("C-x C-r" . 'ido-recentf-open)
+  :custom
+  (recentf-max-saved-items 150)
+  (recentf-max-menu-items 50)
+  :config
+  (recentf-mode 1)
+
+  (defun ido-recentf-open ()
+    "Use `ido-completing-read' to find a recent file."
+    (interactive)
+    (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+	(message "Opening file...")
+      (message "Aborting")))
+  )
