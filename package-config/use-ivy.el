13faf747b13154881713b6e6154d03dad5604a46
Evan Saltman <evan.saltman@gmail.com>
2020-08-11 15:54:02 -0600

    fixed ivy order for fresh installs

--- package-config/use-ivy.el
+++ package-config/use-ivy.el
@@ -1,16 +1,19 @@
-(use-package counsel
-  :diminish
-  :after ivy
-  :config (counsel-mode))
-
 (use-package ivy
+  :ensure t
   :diminish
   :custom
   (ivy-count-format "(%d/%d) ")
   (ivy-use-virtual-buffers t)
   :config (ivy-mode))
 
+(use-package counsel
+  :ensure t
+  :diminish
+  :after ivy
+  :config (counsel-mode))
+
 (use-package swiper
+  :ensure t
   :after ivy
   :bind (("C-s" . swiper)
          ("C-r" . swiper)))
