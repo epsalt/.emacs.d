c88c7db927dbfd29822b817f954af16a93da313d
Evan Saltman <evan.saltman@gmail.com>
2020-08-11 15:54:15 -0600

    fixed org colors to match theme

--- package-config/use-org-mode.el
+++ package-config/use-org-mode.el
@@ -6,7 +6,7 @@
   (org-cycle-separator-lines 1)
   (org-agenda-files '("~/Dropbox/ttd.org"))
   :config
-  (setq my/colors base16-tomorrow-night-colors)
+  (setq my/colors base16-materia-colors)
   (set-face-attribute 'org-document-title nil :weight 'normal :height 1.0)
   ;; (set-face-attribute 'org-todo nil :height 1.0 :foreground (plist-get my/colors :base03))
   (set-face-attribute 'org-level-1 nil :foreground (plist-get my/colors :base0D))
