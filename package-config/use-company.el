ec5c27a5f509a4b254e073d3fcb05d84b64de1f1
Evan Saltman <evan.saltman@gmail.com>
2021-02-04 20:53:02 -0700

    Update company config

--- package-config/use-company.el
+++ package-config/use-company.el
@@ -1,8 +1,10 @@
 (use-package company
   :ensure t
   :diminish
-  :custom
-  (company-idle-delay 0)
-  (company-minimum-prefix-length 1)
   :hook (prog-mode . company-mode)
-  :hook (inferior-python-mode . company-mode))
+  :hook (inferior-python-mode . company-mode)
+  :bind (("C-<tab>" . 'company-complete))
+  :custom
+  (company-idle-delay 0.2)
+  (company-minimum-prefix-length 2)
+)
