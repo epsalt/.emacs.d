9da1a2778a400e0dfebe9f83ec8325d3f41e18a7
Evan Saltman <evan.saltman@gmail.com>
2021-02-04 20:52:23 -0700

    Update rust config

--- package-config/use-rust.el
+++ package-config/use-rust.el
@@ -1,22 +1,43 @@
-(use-package rust-mode
+;; (use-package rust-mode
+;;   :ensure t
+;;   :mode ("\\.rs\\'" . rust-mode)
+;;   :hook (rust-mode . lsp)
+;;   :custom (rust-format-on-save t)
+;;   :init
+;;   (setq lsp-rust-server 'rust-analyzer)
+;;   (setq lsp-prefer-capf t)
+;;   (setq lsp-completion-provider :capf)
+;;   (setq lsp-completion-enable t)
+;;   )
+
+;; (use-package cargo
+;;   :ensure t
+;;   :config
+;;   :hook (rust-mode . cargo-minor-mode))
+
+;; (use-package toml-mode
+;;   :ensure t
+;;   :mode "/\\(Cargo.lock\\|\\.cargo/config\\)\\'")
+
+(use-package rustic
   :ensure t
-  :mode ("\\.rs\\'" . rust-mode)
+  :mode ("\\.rs\\'" . rustic-mode)
   :hook (rust-mode . lsp)
-  :custom (rust-format-on-save t)
-  :init
-  (setq lsp-rust-server 'rust-analyzer))
+  :config
 
-(use-package cargo
+  (setq rustic-lsp-server 'rust-analyzer)
+  (setq lsp-rust-analyzer-server-command '("~/.local/bin/rust-analyzer"))
+  ;; (setq lsp-prefer-capf t)
+  ;; (setq lsp-completion-provider :capf)
+  ;; (setq lsp-completion-enable t)
+
+  (use-package flycheck-rust
   :ensure t
   :config
-  :hook (rust-mode . cargo-minor-mode))
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
-(use-package flycheck-rust
+  (use-package cargo
   :ensure t
   :config
-  (with-eval-after-load 'rust-mode
-    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
-
-(use-package toml-mode
-  :ensure t
-  :mode "/\\(Cargo.lock\\|\\.cargo/config\\)\\'")
+  (cargo-minor-mode))
+  )
