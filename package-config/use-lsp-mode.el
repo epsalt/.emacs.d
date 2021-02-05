04e4dfafdf23ae69e02507aa35693a1792fa170a
Evan Saltman <evan.saltman@gmail.com>
2021-02-04 20:51:06 -0700

    Remove lsp-company

--- package-config/use-lsp-mode.el
+++ package-config/use-lsp-mode.el
@@ -11,14 +11,3 @@
   (lsp-eldoc-prefer-signature-help t)
   (lsp-response-timeout 5)
   (lsp-prefer-flymake :none))
-
-(use-package company-lsp
-  :ensure t
-  :commands company-lsp
-  :after (lsp-mode company)
-  :custom
-  (company-lsp-async t)
-  (company-lsp-cache-candidates 'auto)
-  (company-lsp-enable-recompletion t)
-  :config
-  (push 'company-lsp company-backends))
