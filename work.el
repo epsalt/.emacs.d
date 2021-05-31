58dcaa837b5606427f56ba7f5250cc4dd18b5dfc
Evan Saltman <evan.saltman@gmail.com>
2021-05-30 22:04:19 -0600

    New work branch

---
+++ work.el
@@ -0,0 +1,52 @@
+;; Black
+(remove-hook 'python-mode-hook 'blacken-mode)
+(add-hook 'black-mode-hook 'my/black-config-hook)
+
+(defun my/black-config-hook ()
+  (setq blacken-skip-string-normalization t)
+  (setq blacken-line-length 99))
+
+;; Flycheck
+(add-hook 'flycheck-mode-hook 'my/flycheck-config-hook)
+(add-hook 'js-mode-hook 'my/flycheck-js-config-hook)
+
+(defun my/flycheck-config-hook ()
+  (setq flycheck-python-flake8-executable "/home/epsalt/.local/bin/flake8")
+  (setq flycheck-flake8rc "~/.flake8")
+  (setq-default flycheck-disabled-checkers '(python-pylint))
+  (setq flycheck-help-echo-function nil))
+
+(defun my/flycheck-js-config-hook ()
+  (flycheck-select-checker 'javascript-tide))
+
+;; Javascript
+(remove-hook 'js-mode-hook 'prettier-js-mode)
+(add-hook 'js-mode-hook 'my/js-indent-hook)
+(add-hook 'json-mode-hook 'my/js-indent-hook)
+
+(defun my/js-indent-hook ()
+  (setq js-indent-level 4))
+
+;; Org
+(add-hook 'org-mode-hook 'my/org-directory-hook)
+
+(defun my/org-directory-hook ()
+  (setq org-agenda-files '("~/org/todo.org"))
+  (setq org-directory '("~/org/")))
+
+;; Projectile
+(add-hook 'projectile-mode-hook 'my/projectile-path-hook)
+
+(defun my/projectile-path-hook ()
+  (setq projectile-project-search-path '("~/code")))
+
+;; Web mode
+(add-hook 'web-mode-hook 'my/web-mode-indentation-hook)
+(add-hook 'editorconfig-custom-hooks my/editorconfig-script-padding-hook)
+
+(defun my/web-mode-indentation-hook ()
+  (setq web-mode-enable-control-block-indentation nil)
+  (setq web-mode-script-padding 0))
+
+(defun my/editorconfig-script-padding-hook (hash)
+  (setq web-mode-script-padding 0))
