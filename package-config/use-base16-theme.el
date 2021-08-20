f8e7018d1d29250717a6886aaf06504029049ec1
Evan Saltman <evan.saltman@gmail.com>
2021-08-20 14:53:10 -0600

    Switch to tomorrow night theme

--- package-config/use-base16-theme.el
+++ package-config/use-base16-theme.el
@@ -4,5 +4,5 @@
   (setq-default base16-distinct-fringe-background nil)
   :config
   (setq base16-theme-256-color-source "terminal")
-  (load-theme 'base16-materia t)
+  (load-theme 'base16-tomorrow-night)
   )
