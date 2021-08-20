73469b97bf38a3cba6734a56a3ca302f05fae3b2
Evan Saltman <evan.saltman@gmail.com>
2021-08-20 14:52:05 -0600

    Update consult-ripgrep command

--- package-config/use-vertico.el
+++ package-config/use-vertico.el
@@ -25,7 +25,10 @@
          ("C-c C-j" . consult-imenu))
   :hook (completion-list-mode . consult-preview-at-point-mode)
   :config
-  (setq consult-project-root-function #'projectile-project-root))
+  (setq consult-project-root-function #'projectile-project-root)
+  (setq consult-ripgrep-command "rg --null --line-buffered --color=ansi --max-columns=1000\
+  --hidden --glob=!vendor/ --glob=!.git/ --glob=!*.svg --smart-case --no-heading --line-number . -e ARG OPTS")
+  )
 
 (use-package marginalia
   :ensure t
