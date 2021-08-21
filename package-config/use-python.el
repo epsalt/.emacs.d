6c9fb7564169920b859c792de761295b061c15c4
Evan Saltman <evan.saltman@gmail.com>
2021-08-20 22:59:36 -0600

    Clean up python config a bit

--- package-config/use-python.el
+++ package-config/use-python.el
@@ -2,43 +2,38 @@
   :demand t
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode)
-  :hook (inferior-python-mode . my-python-enable-eldoc-inferior-python)
+  :hook (inferior-python-mode . my/python-enable-eldoc-inferior-python)
   :bind (:map python-mode-map
               ("C-c C-j" . consult-imenu))
+  :custom
+  (python-environment-directory (expand-file-name "~/.virtualenvs"))
+  (python-shell-interpreter "python3")
   :config
-  (setq python-environment-directory (expand-file-name "~/.virtualenvs"))
-  (setq python-shell-interpreter "python3")
-
   (defcustom ipython-shell-interpreter-args "-m IPython --simple-prompt --pprint -i"
     "Default arguments for the IPython interpreter."
     :type 'string
     :group 'python)
-
   (defun ipython-shell-calculate-command ()
     (format "%s %s"
             (combine-and-quote-strings (list python-shell-interpreter))
             ipython-shell-interpreter-args))
-
   (defun run-ipython ()
     (interactive)
     (run-python (ipython-shell-calculate-command) nil t))
-
-  (defun my-python-enable-eldoc-inferior-python ()
+  (defun my/python-enable-eldoc-inferior-python ()
     (setq-local eldoc-documentation-function #'python-eldoc-function))
+  )
 
-  (use-package company-jedi
-    :ensure t
-    :custom
-    (jedi:complete-on-dot t)
-    (jedi:tooltip-method nil)
-    (jedi:setup-keys t)
-    :config
-    (add-hook 'python-mode-hook 'jedi:setup)
-    (defun my/python-mode-hook ()
-      (add-to-list 'company-backends 'company-jedi))
-    (add-hook 'python-mode-hook 'my/python-mode-hook)
-
-    (setq jedi:environment-virtualenv
-          (append python-environment-virtualenv '("--python" "python3"))))
-
+(use-package company-jedi
+  :ensure t
+  :hook ((python-mode . jedi:setup)
+         (python-mode . my/python-mode-hook))
+  :custom
+  (jedi:complete-on-dot t)
+  (jedi:tooltip-method nil)
+  (jedi:setup-keys t)
+  (jedi:environment-virtualenv (append python-environment-virtualenv '("--python" "python3")))
+  :config
+  (defun my/python-mode-hook ()
+      (add-to-list 'company-backends  'company-jedi))
   )
