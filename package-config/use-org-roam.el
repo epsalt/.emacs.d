fa8a0126ab64d595dc6b417b509366f0160738ab
Evan Saltman <evan.saltman@gmail.com>
2021-08-21 12:47:35 -0600

    More org-roam migration

--- package-config/use-org-roam.el
+++ package-config/use-org-roam.el
@@ -1,34 +1,30 @@
 (use-package org-roam
   :ensure t
-  :diminish org-roam-mode
   :hook
-  (after-init . org-roam-mode)
+  (after-init . org-roam-setup)
   (org-mode . company-mode)
   :custom
   (org-roam-directory "~/roam")
   (org-roam-dailies-directory "daily/")
-  (org-adapt-indentation 'nil)
-  :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ("C-c n g" . org-roam-graph)
-               ("C-c n c" . org-roam-dailies-capture-today)
-               ("C-c n t" . org-roam-dailies-find-today))
-
-              :map org-mode-map
-              (("C-c n i" . org-roam-insert))
-              (("C-c n I" . org-roam-insert-immediate)))
+  :bind (("C-c n l" . org-roam)
+         ("C-c n f" . org-roam-find-file)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n c" . org-roam-dailies-capture-today)
+         ("C-c n t" . org-roam-dailies-find-today)
+         :map org-mode-map
+         ("C-c n i" . org-roam-insert)
+         ("C-c n I" . org-roam-insert-immediate))
+  :init
+  (setq org-roam-v2-ack t)
   :config
+  (org-roam-db-autosync-mode)
   (add-hook 'org-mode-hook
             (lambda ()
               (make-local-variable 'company-backends)
               (setq company-backends '(company-capf))))
-
   (setq org-roam-dailies-capture-templates
-        '(("d" "default" entry
-           #'org-roam-capture--get-point
-           "* %?"
-           :file-name "daily/%<%Y-%m-%d>"
-           :head "#+title: %<%Y-%m-%d>\n\n")))
-
+      '(("d" "default" entry
+         "* %?"
+         :if-new (file+head "%<%Y-%m-%d>.org"
+                            "#+title: %<%Y-%m-%d>\n"))))
   )
