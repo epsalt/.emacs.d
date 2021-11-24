57269493a2796dbb7258be878a2eb71b4f884524
Evan Saltman <evan.saltman@gmail.com>
2021-11-23 21:28:16 -0700

    Use eslint over lsp-mode checker

--- package-config/use-flycheck.el
+++ package-config/use-flycheck.el
@@ -3,4 +3,14 @@
   :init (global-flycheck-mode)
   :config
   (setq flycheck-highlighting-mode 'nil)
+  (defun my/use-eslint-from-node-modules ()
+    (let* ((root (locate-dominating-file
+                  (or (buffer-file-name) default-directory)
+                  "node_modules"))
+           (eslint (and root
+                        (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                          root))))
+      (when (and eslint (file-executable-p eslint))
+        (setq-local flycheck-javascript-eslint-executable eslint))))
+  (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
   )
