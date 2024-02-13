b156504f8ab879b515128e61f317883234080569
Evan Saltman <evan.saltman@gmail.com>
2024-02-13 08:49:12 -0700

    Switch from lsp to eglot

--- package-config/use-js.el
+++ package-config/use-js.el
@@ -4,24 +4,23 @@
 
 (use-package js
   :ensure nil
-  :hook (js-mode . lsp)
+  :hook (js-mode . eglot-ensure)
   :custom
-  (js-indent-level 2)
-  (lsp-diagnostics-disabled-modes '(js-mode))
-  (lsp-modeline-diagnostics-enable nil))
+  (js-indent-level 2))
 
 (use-package typescript-mode
   :ensure t
   :mode "\\.ts\\'"
-  :hook (typescript-mode . lsp)
+  :hook (typescript-mode . eglot-ensure)
   :custom
   (typescript-indent-level 2))
 
 (use-package prettier-js
   :ensure t
   :hook ((js-mode typescript-mode web-tsx-mode web-jsx-mode) . prettier-js-mode)
-  :custom
-  (prettier-js-command "prettier"))
+  :custom (prettier-js-command "prettier")
+  :config
+  (add-hook 'json-mode-hook (lambda () (prettier-js-mode -1))))
 
 (use-package web-mode
   :ensure t
@@ -46,16 +45,20 @@
   (web-mode-enable-auto-quoting nil))
 
 (use-package web-vue-mode
-  :hook (web-vue-mode . lsp))
+  :hook (web-vue-mode . eglot-ensure)
+  )
 
 (use-package web-tsx-mode
-  :hook (web-tsx-mode . lsp))
+  :hook (web-tsx-mode . eglot-ensure)
+  )
 
 (use-package web-jsx-mode
-  :hook (web-jsx-mode . lsp))
+  :hook (web-jsx-mode . eglot-ensure)
+
+  )
 
 (use-package web-vue-mode
-  :hook (web-vue-mode . lsp)
+  :hook (web-vue-mode . eglot-ensure)
   :custom
   (web-mode-script-padding 0)
   (web-mode-style-padding 0))
