a1c8ccca8ce21fe827b0bdf7e4b0ca7346083465
epsalt <evan.saltman@gmail.com>
2024-12-10 14:24:01 -0700

    Eglot / flymake improvements for python/go/js

--- package-config/use-eglot.el
+++ package-config/use-eglot.el
@@ -1,33 +1,33 @@
 (use-package eglot
-  :ensure t
   :bind (:map eglot-mode-map
               ("C-c ?" . eldoc-doc-buffer)
               ("C-c ." . xref-find-definitions)
               ("C-c ," . xref-pop-marker-stack)
               ("C-c r" . xref-find-references))
+  :hook
+  (eglot--managed-mode . manually-activate-flymake)
+  :custom
+  (eglot-events-buffer-size 0)
+  (eglot-sync-connect nil)
+  (eglot-ignored-server-capabilities
+   '(:documentHighlightProvider
+     :codeActionProvider
+     :documentSymbolProvider
+     :workspaceSymbolProvider
+     :codeLensProvider
+     :documentFormattingProvider
+     :documentRangeFormattingProvider
+     :documentOnTypeFormattingProvider
+     :renameProvider
+     :documentLinkProvider
+     :colorProvider
+     :foldingRangeProvider
+     :inlayHintProvider))
+  :preface
+  (defun manually-activate-flymake ()
+    (add-hook 'flymake-diagnostic-functions #'eglot-flymake-backend nil t)
+    (flymake-mode 1))
   :config
   (fset #'jsonrpc--log-event #'ignore)
-  (setq eglot-events-buffer-size 0)
-  (setq eglot-sync-connect nil)
-
-  (setq eglot-ignored-server-capabilities
-        '(:documentHighlightProvider
-          :codeActionProvider
-          :documentSymbolProvider
-          :workspaceSymbolProvider
-          :codeLensProvider
-          :documentFormattingProvider
-          :documentRangeFormattingProvider
-          :documentOnTypeFormattingProvider
-          :renameProvider
-          :documentLinkProvider
-          :colorProvider
-          :foldingRangeProvider
-          :inlayHintProvider))
-
-  (add-to-list 'eglot-server-programs
-               '(web-tsx-mode . ("typescript-language-server" "--stdio")))
-  (add-to-list 'eglot-server-programs
-               '(web-jsx-mode . ("typescript-language-server" "--stdio")))
-  (add-to-list 'eglot-server-programs
-               '(python-ts-mode . ("pyright-langserver" "--stdio"))))
+  (add-to-list 'eglot-server-programs '(python-ts-mode . ("jedi-language-server")))
+  (add-to-list 'eglot-stay-out-of 'flymake))
--- package-config/use-pyvenv.el
+++ package-config/use-pyvenv.el
@@ -1,6 +1,7 @@
 (use-package pyvenv
   :ensure t
   :commands (pyvenv-workon pyvenv-activate)
-  :hook (python-mode . pyvenv-tracking-mode)
+  :hook (python-ts-mode . pyvenv-tracking-mode)
   :config
+  (setenv "WORKON_HOME" "~/.virtualenvs")
   (pyvenv-mode))
