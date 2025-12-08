2fd00c39ab8f6c98ef491af050b9d1877c450690
epsalt <evan.saltman@gmail.com>
2025-12-08 11:28:00 -0700

    Python config updates ruff/apheleia

--- package-config/use-apheleia.el
+++ package-config/use-apheleia.el
@@ -1,9 +1,7 @@
 (use-package apheleia
   :ensure t
-  :hook (after-init . apheleia-global-mode)
   :custom
   (apheleia-mode-lighter nil)
   :config
   (setf (alist-get 'python-mode apheleia-mode-alist) '(ruff-isort ruff))
-  (setf (alist-get 'python-ts-mode apheleia-mode-alist) '(ruff-isort ruff))
-  (apheleia-global-mode t))
+  (setf (alist-get 'python-ts-mode apheleia-mode-alist) '(ruff-isort ruff)))
--- package-config/use-python.el
+++ package-config/use-python.el
@@ -1,19 +1,7 @@
-;; from https://github.com/fast-90/dotfiles/blob/main/repos/config-files/emacs/config.org
-(defun find-ruff-config-files-in-current-project ()
-  "Search for .ruff.toml, ruff.toml, or pyproject.toml in the project root of the current buffer."
-  (when (project-current)
-    (let* ((project-root (project-root (project-current)))  ;; Automatically find project root
-           (files '(".ruff.toml" "ruff.toml" "pyproject.toml")))
-      (if project-root
-          (cl-loop for file in files
-                   for filepath = (expand-file-name file project-root)
-                   when (file-exists-p filepath)
-                   return filepath)
-        nil))))
-
 (use-package python
   :mode ("\\.py\\'" . python-ts-mode)
   :hook ((python-ts-mode . flymake-mode)
+         (python-ts-mode . apheleia-mode)
          (python-ts-mode . eglot-ensure)
          (python-ts-mode . my/python-hook))
   :interpreter ("python" . python-ts-mode)
@@ -26,4 +14,4 @@
      1 2 nil 3))
   :preface
   (defun my/python-hook ()
-    (setq-local python-flymake-command `("ruff" "check" "--output-format" "pylint" "--config" ,(find-ruff-config-files-in-current-project) "--quiet" "--stdin-filename" ,buffer-file-name "-"))))
+  (setq-local python-flymake-command `("ruff" "check" "--output-format" "pylint" "--quiet" "--stdin-filename" ,buffer-file-name "-"))))
